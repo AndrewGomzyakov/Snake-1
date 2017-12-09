f07b69c506d533ef58365e4a0e512d276d310b7c
Ollisteka <zhukova.o.m@yandex.ru>
2017-12-09 19:20:12 +0500

    разбила код на методы

--- src/endlessLevel/LevelCreator.java
+++ src/endlessLevel/LevelCreator.java
@@ -1,100 +1,120 @@
 package endlessLevel;
 
+import gameCore.GameState;
 import java.util.Random;
 
-import direction.Dir;
-import gameCore.GameState;
-
 public class LevelCreator {
-	private GameState gameSate;
-	private int r;
-	private int l;
-	private int u;
-	private int d;
-	public LevelCreator(GameState gameState) {
-		this.gameSate = gameState;
-		r = gameState.getMap().length / 2 - 6;
-		l = r;
-		u = gameState.getMap()[0].length / 2 - 6;
-		d = u;
-	}
-	
-	public void updateLevel() {
-		if (this.gameSate.getSnake().getDir().getDir() == Dir.Right) {
-			if (r > 0) {
-				r--;
-				l++;
-			}
-			else {
-				updateMap(1, (this.gameSate.getHead(this.gameSate.getSnake()).x + 1 + 6) % this.gameSate.getMap()[0].length);
-			}
-		}
-		if (this.gameSate.getSnake().getDir().getDir() == Dir.Left) {
-			if (l > 0) {
-				l--;
-				r++;
-			}
-			else {
-				updateMap(1, (this.gameSate.getHead(this.gameSate.getSnake()).x - 1 - 6 + this.gameSate.getMap()[0].length) % this.gameSate.getMap()[0].length);
-			}
-		}
-		if (this.gameSate.getSnake().getDir().getDir() == Dir.Up) {
-			if (u > 0) {
-				u--;
-				d++;
-			}
-			else {
-				updateMap(0, (this.gameSate.getHead(this.gameSate.getSnake()).y + 7) % this.gameSate.getMap().length);
-			}
-		}
-		if (this.gameSate.getSnake().getDir().getDir() == Dir.Down) {
-			if (d > 0) {
-				d--;
-				u++;
-			}
-			else {
-				updateMap(0, (this.gameSate.getHead(this.gameSate.getSnake()).y - 7 + this.gameSate.getMap().length) % this.gameSate.getMap().length);
-			}
-		}
-		
-	}
-	private void updateMap(int type, int cord) {
-		if (type == 1) {
-			
-			for (int i = 0; i < this.gameSate.getMaze().length; i++) {
-				this.gameSate.getMaze()[i][cord] = '.';
-			}
-			char[][] a = this.gameSate.getMap();
-			for (int i = 0; i < this.gameSate.getMaze().length; i++) {
-				if (a[i][cord] == '.') {
-					Random rnd = new Random();
-					int ch = rnd.nextInt(100);
-					if (ch <= 10) {
-						this.gameSate.getMaze()[i][cord] = '#';
-					}
-					if (ch > 10 && ch <= 20) {
-						this.gameSate.getMaze()[i][cord] = '+';
-					}
-				}
-			}
-		}
-		else {
-			for (int i = 0; i < this.gameSate.getMaze()[0].length; i++) {
-				this.gameSate.getMaze()[cord][i] = '.';
-			}
-			char[][] a = this.gameSate.getMap();
-			for (int i = 0; i < this.gameSate.getMaze()[0].length; i++) {
-				if (a[cord][i] == '.') {
-					Random rnd = new Random();
-					int ch = rnd.nextInt(100);
-					if (ch <= 10) {
-						this.gameSate.getMaze()[cord][i] = '#';
-					}
-					if (ch > 10 && ch <= 20) {
-						this.gameSate.getMaze()[cord][i] = '+';
-					}
-				}
-			}
-		}
-	}
+
+  private GameState gameState;
+  private int r;
+  private int l;
+  private int u;
+  private int d;
+
+  public LevelCreator(GameState gameState) {
+    this.gameState = gameState;
+    r = gameState.getMap().length / 2 - 6;
+    l = r;
+    u = gameState.getMap()[0].length / 2 - 6;
+    d = u;
+  }
+
+  public void updateLevel() {
+    switch (this.gameState.getSnake().getDir().getDir()) {
+      case Up:
+        if (u > 0) {
+          u--;
+          d++;
+        } else {
+          updateMap(0, (this.gameState.getHead(this.gameState.getSnake()).y + 7)
+              % this.gameState.getMap().length);
+        }
+        break;
+      case Down:
+        if (d > 0) {
+          d--;
+          u++;
+        } else {
+          updateMap(0, (this.gameState.getHead(this.gameState.getSnake()).y - 7
+              + this.gameState.getMap().length)
+              % this.gameState.getMap().length);
+        }
+        break;
+      case Left:
+        if (l > 0) {
+          l--;
+          r++;
+        } else {
+          updateMap(1, (this.gameState.getHead(this.gameState.getSnake()).x - 1 - 6
+              + this.gameState.getMap()[0].length)
+              % this.gameState.getMap()[0].length);
+        }
+        break;
+      case Right:
+        if (r > 0) {
+          r--;
+          l++;
+        } else {
+          updateMap(1, (this.gameState.getHead(this.gameState.getSnake()).x + 1 + 6)
+              % this.gameState.getMap()[0].length);
+        }
+        break;
+    }
+  }
+
+  private void deleteOldWalls(int maxLength, int coordinate, boolean swapValues) {
+    int x;
+    int y;
+    for (int i = 0; i < maxLength; i++) {
+      if (swapValues) {
+        x = coordinate;
+        y = i;
+      } else {
+        x = i;
+        y = coordinate;
+      }
+      this.gameState.getMaze()[x][y] = '.';
+    }
+  }
+
+  private char generateWall() {
+    Random rnd = new Random();
+    int ch = rnd.nextInt(100);
+    if (ch <= 10) {
+      return '#';
+    }
+    if (ch <= 20) {
+      return '+';
+    }
+    return '.';
+  }
+
+  private void createNewWalls(int maxLength, int coordinate, boolean swapValues) {
+    deleteOldWalls(maxLength, coordinate, swapValues);
+    char[][] map = this.gameState.getMap();
+    int x;
+    int y;
+    for (int i = 0; i < maxLength; i++) {
+      if (swapValues) {
+        x = coordinate;
+        y = i;
+      } else {
+        x = i;
+        y = coordinate;
+      }
+      if (map[x][y] != '.') {
+        continue;
+      }
+      this.gameState.getMaze()[x][y] = generateWall();
+
+    }
+  }
+
+  private void updateMap(int type, int coordinate) {
+    if (type == 1) {
+      createNewWalls(this.gameState.getMaze().length, coordinate, false);
+    } else {
+      createNewWalls(this.gameState.getMaze()[0].length, coordinate, true);
+    }
+  }
 }
